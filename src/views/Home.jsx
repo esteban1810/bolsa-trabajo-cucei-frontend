a3fd833c3fb17a5a00df9a4f92917f0c787550c0
esteban1810 <estebansevillapg@gmail.com>
2024-06-23 00:22:19 -0600

    [UPDATE]

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -4,13 +4,6 @@ export default function Home() {
     return (
         <div>
             <div>
-                <iframe
-                    src="https://www.youtube.com/embed/ZqqEPWe1Ad0?si=I8fNbledssVRsXDe"
-                    title="YouTube video player"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                    referrerpolicy="strict-origin-when-cross-origin"
-                    allowfullscreen
-                ></iframe>
             </div>
         </div>
     );
