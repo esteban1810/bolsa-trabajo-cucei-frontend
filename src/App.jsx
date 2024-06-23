3c3a200981e53c3855298a97109ee84fcdd2a1c9
esteban1810 <estebansevillapg@gmail.com>
2024-06-22 20:02:22 -0600

    [ADD] routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
-// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-// import Header from './components/layouts/Header';
-// import Footer from './components/layouts/Footer';
-// import Login from './components/Login';
-import Login from './views/Login'
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
 import './styles/App.css';
+import Layout from './components/layout/Layout'
+import Login from './views/Login'
+import Register from './views/Register'
 
 function App() {
   return (
-    <Login/>
+    <Router>
+      <Layout>
+        <Routes>
+          {/* <Route path="/" element={<Home />} /> */}
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/register' element={<Register/>}/>
+        </Routes>
+      </Layout>
+    </Router>
   );
 }
 
