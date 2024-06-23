1950efa9eb50735a099fef9f9a61a6138be3c6b4
esteban1810 <estebansevillapg@gmail.com>
2024-06-22 21:28:25 -0600

    [UPDATE] HEADER

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
       <img src={logo} alt="Universidad de Guadalajara" className="mx-auto" />
       <div className="bg-primary py-1">
         <div className="custom-width flex justify-between items-center">
-          <h2 className="text-[1.4rem] font-bold text-white">Centro Universitario de Ciencias Exactas e Ingenierías</h2>
+          <h2 className="text-[1.4rem] font-bold text-white">Bolsa De Trabajo CUCEI</h2>
           <i className="material-icons text-5xl text-white">account_circle</i>
         </div>
       </div>
@@ -15,6 +15,7 @@ const Header = () => {
         <a href="google.com">Inicio</a>
         <a href="google.com">Buscar Empleo</a>
         <a href="google.com">Postular Empleo</a>
+        <a href="google.com">Crear Curriculum</a>
         <a href="google.com">Participar en la feria del empleo</a>
       </div>
     </header>
