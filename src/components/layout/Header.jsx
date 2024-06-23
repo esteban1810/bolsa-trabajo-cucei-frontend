f5118f8901a472217ba684451d38ffc01110ad0a
esteban1810 <estebansevillapg@gmail.com>
2024-06-22 20:53:13 -0600

    [ADD] menu options

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -3,15 +3,19 @@ import logo from '../../images/logo_udg.png';
 
 const Header = () => {
   return (
-    <header>
-      <div className="bg-white">
-        <img src={logo} alt="Universidad de Guadalajara" className="mx-auto" />
-      </div>
-      <div className="bg-primary flex items-center py-1">
+    <header className='bg-white'>
+      <img src={logo} alt="Universidad de Guadalajara" className="mx-auto" />
+      <div className="bg-primary py-1">
         <div className="custom-width flex justify-between items-center">
           <h2 className="text-[1.4rem] font-bold text-white">Centro Universitario de Ciencias Exactas e Ingenierías</h2>
           <i className="material-icons text-5xl text-white">account_circle</i>
         </div>
+      </div>
+      <div className='custom-width bg-white flex justify-between'>
+        <a href="google.com">Inicio</a>
+        <a href="google.com">Buscar Empleo</a>
+        <a href="google.com">Postular Empleo</a>
+        <a href="google.com">Participar en la feria del empleo</a>
       </div>
     </header>
   );
