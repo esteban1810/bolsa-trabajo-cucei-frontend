7d117e74d0c14872029e881c2223eb722d206cd8
esteban1810 <estebansevillapg@gmail.com>
2024-06-22 23:17:41 -0600

    [ADD] MENU

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,30 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../../images/logo_udg.png';
-import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
 
-
-
 const Header = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <header className='bg-white'>
       <img src={logo} alt="Universidad de Guadalajara" className="mx-auto" />
       <div className="bg-primary py-1">
         <div className="custom-width flex justify-between items-center">
-          <h2 className="text-[2rem] font-bold text-white">Bolsa De Trabajo CUCEI</h2>
-          <div>
-            <AccountCircleOutlinedIcon className='text-white mr-4 h-full' sx={{ fontSize: 40 }} />
-            <MenuOutlinedIcon className='text-white m-0' sx={{ fontSize: 50 }}/>
+          <h2 className="text-2xl font-bold text-white">Bolsa De Trabajo CUCEI</h2>
+          <div className="relative">
+            <MenuOutlinedIcon onClick={toggleMenu} className='text-white m-0 cursor-pointer' sx={{ fontSize: 50 }}/>
+            {showMenu && (
+              <ul className="absolute bg-white mt-2 rounded shadow-md w-48 right-0">
+                <li className="whitespace-nowrap py-1 px-2 pt-2 hover:bg-primary hover:text-white"><a href="/register">Inicio</a></li>
+                <li className="whitespace-nowrap py-1 px-2 hover:bg-primary hover:text-white"><a href="/register">Iniciar Sesión</a></li>
+                <li className="whitespace-nowrap py-1 px-2 hover:bg-primary hover:text-white"><a href="/register">Registrarse</a></li>
+                <li className="whitespace-nowrap py-1 px-2 hover:bg-primary hover:text-white"><a href="/register">Cerrar Sesión</a></li>
+                <li className="whitespace-nowrap py-1 px-2 hover:bg-primary hover:text-white"><a href="/register">Buscar Empleo</a></li>
+                <li className="whitespace-nowrap py-1 px-2 hover:bg-primary hover:text-white"><a href="/register">Postular Empleo</a></li>
+                <li className="whitespace-nowrap py-1 px-2 hover:bg-primary hover:text-white"><a href="/register">Agendar Visita</a></li>
+                <li className="whitespace-nowrap py-1 px-2 hover:bg-primary hover:text-white"><a href="/register">Crear Curriculum</a></li>
+                <li className="whitespace-nowrap py-1 px-2 pb-2 hover:bg-primary hover:text-white"><a href="/register">Feria del empleo</a></li>
+              </ul>
+            )}
           </div>
         </div>
-      </div>
-      <div className='custom-width bg-white flex justify-between'>
-        <a href="google.com">Inicio</a>
-        <a href="google.com">Buscar Empleo</a>
-        <a href="google.com">Postular Empleo</a>
-        <a href="google.com">Agendar Visita</a>
-        <a href="google.com">Crear Curriculum</a>
-        <a href="google.com">Participar en la feria del empleo</a>
       </div>
     </header>
   );
